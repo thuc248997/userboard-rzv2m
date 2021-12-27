7d2c80e8d2396ec0396a06f04008f14e16b46f38
Lexra Chang <jason.chang@digitalmediacartridge.com>
2021-12-27 21:54:02 +0800

    proprietary check

--- build.sh
+++ build.sh
@@ -54,7 +54,11 @@ WORK=`pwd`
 #
 sudo umount mnt || true
 mkdir -p mnt && sudo rm -rfv mnt/*
-#[ ! -f proprietary/RTM0AC0000XV264D30SL41C_3_0_17.zip ] && exit 1
+[ ! -f proprietary/rzv2m_drpai-sample-application_ver5.00.tar.gz ] && exit 1
+[ ! -f proprietary/rzv2m_isp_support-pkg_v110.tar.gz ] && exit 1
+[ ! -f proprietary/rzv2m_meta-drpai_ver5.00.tar.gz ] && exit 1
+[ ! -f proprietary/core1_firmware.bin ] && exit 1
+[ ! -x proprietary/DRP-AI_Translator-v1.60-Linux-x86_64-Install ] && exit 1
 sudo chown -R ${USER}.${USER} ${WORK}/proprietary
 
 ##########################################################
